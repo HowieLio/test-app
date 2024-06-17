fed455e208e7f24c0b3070306cc89f47084bcf23
HowieLio <122736519+HowieLio@users.noreply.github.com>
2024-06-17 14:46:55 +0300

    Second commit. Nav-bar is ready

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,24 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
+    <div class="topbar">
+        <div class="sections">
+            <a class="active">Продукты</a>
         </div>
     </div>
+        XUI
 </x-app-layout>
+<style>
+    .topbar {
+        position: relative;
+        top: 0;
+        left: 0;
+        width: 100%;
+        box-sizing: border-box;
+        padding: 20px;
+        background-color: white;
+    }
+    .sections {
+        align-items: center;
+        display: flex;
+        gap: 20px;
+    }
+</style>
