7fb12e0017dd1f01907126c303206e33b62e507d
HowieLio <122736519+HowieLio@users.noreply.github.com>
2024-06-17 17:06:49 +0300

    Fourth commit. List

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function view()
     {
+        /* @var User $user */
         $user = auth()->user();
-        return view('products.list', ['user' => $user]);
+        /* @var Product $products */
+        $products = Product::all();
+        return view('products.list', ['user' => $user, 'products' => $products]);
     }
 }
--- resources/views/products/list.blade.php
+++ resources/views/products/list.blade.php
@@ -9,8 +9,39 @@
             </a>
         </div>
     </div>
+    <button class="add-button">
+        Добавить
+    </button>
+
     <table>
-        XUI
+        <thead>
+        <tr>
+            <th>Артикул</th>
+            <th>Название</th>
+            <th>Статус</th>
+            <th>Атрибуты</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($products as $product)
+            <tr class="tr-body">
+                <th>{{ $product->article }}</th>
+                <th>{{ $product->name }}</th>
+                <th>{{ $product->status === 'available' ? 'Доступен' : 'Не доступен' }}</th>
+                <th>
+                    @foreach(json_decode($product->data, true) as $key => $value)
+                        <span>
+                            {{ $key }}:
+                        </span>
+                        <span>
+                            {{ $value }}
+                        </span>
+                        <br>
+                    @endforeach
+                </th>
+            </tr>
+        @endforeach
+        </tbody>
     </table>
 </x-app-layout>
 <style>
@@ -20,7 +51,7 @@
         left: 0;
         width: 100%;
         box-sizing: border-box;
-        padding: 20px;
+        padding: 0 20px;
         background-color: white;
     }
     .sections {
@@ -32,6 +63,40 @@
             display: flex;
             align-items: center;
             gap: 20px;
+            a {
+                padding: 25px 0;
+                text-transform: uppercase;
+            }
+            a.active {
+                color: #ED1C24;
+                border-bottom: 4px solid #ED1C24;
+            }
         }
+    }
+    table {
+        width: 70%;
+        border-collapse: collapse;
+        margin-top: 20px;
+    }
+    th, td {
+        padding: 8px;
+        text-align: left;
+        font-weight: 400;
+        color: #6E6E6F;
+        border-bottom: 1px solid #C4C4C4;
+    }
+    tr.tr-body {
+        box-sizing: border-box;
+        padding: 10px;
+        background-color: white;
+    }
+    .add-button {
+        background-color: #0dc5fe;
+        position: absolute;
+        right: 20px;
+        top: 100px;
+        color: white;
+        padding: 5px 40px;
+        border-radius: 10px;
     }
 </style>
