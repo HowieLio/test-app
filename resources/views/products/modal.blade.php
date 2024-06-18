c1992e9811609ffe37c6f139a51f80d0368cf71e
HowieLio <122736519+HowieLio@users.noreply.github.com>
2024-06-18 13:49:08 +0300

    Safe pos to add new file

--- resources/views/products/modal.blade.php
+++ resources/views/products/modal.blade.php
@@ -1,149 +1,50 @@
 <x-app-layout>
-    <div class="topbar">
-        <div class="sections">
-            <div class="section">
-                <a href="{{ route('products.list') }}" class="active">Продукты</a>
-            </div>
-            <a href="{{ route('profile.edit') }}">
-               {{ $user->name }}
-            </a>
-        </div>
-    </div>
-    <div class="table-content">
-        <table>
-            <thead>
-            <tr>
-                <th>Артикул</th>
-                <th>Название</th>
-                <th>Статус</th>
-                <th>Атрибуты</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($products as $product)
-                <tr class="tr-body">
-                    <th>{{ $product->article }}</th>
-                    <th>{{ $product->name }}</th>
-                    <th>{{ $product->status === 'available' ? 'Доступен' : 'Не доступен' }}</th>
-                    <th>
-                        @foreach(json_decode($product->data, true) as $key => $value)
-                            <span>
-                            {{ $key }}:
-                        </span>
-                            <span>
-                            {{ $value }}
-                        </span>
-                            <br>
-                        @endforeach
-                    </th>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
-        <button id="add-button" onclick="handleAddButton()" class="add-button">
-            Добавить продукт
-        </button>
-        <div id="myModal" class="modal">
-            <div class="modal-content">
-                <form id="add-block" class="add-block" action="{{ route('products.create') }}" method="POST">
-                    @csrf
-                    <span class="close" onclick="closeModal()">
-                        &times;
-                    </span>
-                    <h1 style="color: white; font-size: 20px; font-weight: 900; margin-bottom: 20px">
-                        Добавить продукт
-                    </h1>
-                    <div class="input-box">
-                        <label>
-                            Артикул
-                        </label>
-                        <input id="article" name="article" type="text" required>
-                    </div>
-                    <div class="input-box">
-                        <label>
-                            Название
-                        </label>
-                        <input id="name" name="name" type="text" required>
-                    </div>
-                    <div class="input-box">
-                        <label>
-                            Статус
-                        </label>
-                        <select id="status" name="status">
-                            <option value="available">Доступен</option>
-                            <option value="unavailable">Недоступен</option>
-                        </select>
-                        <label style="font-weight: 900; margin-top: 10px">
-                            Атрибуты
-                        </label>
-                    </div>
-                    <div id="attributesInputContainer"></div>
-                    <button type="button" onclick="generateInputs()" class="add-attribute">
-                        + Добавить атрибут
-                    </button>
-                    <button type="submit" class="save-button">
-                        Добавить
-                    </button>
-                </form>
-            </div>
+    <div id="myModal" class="modal">
+        <div class="modal-content">
+            <form id="add-block" class="add-block" action="{{ route('products.create') }}" method="POST">
+                @csrf
+                <span class="close" onclick="closeModal()">
+                    &times;
+                </span>
+                <h1 style="color: white; font-size: 20px; font-weight: 900; margin-bottom: 20px">
+                    Добавить продукт
+                </h1>
+                <div class="input-box">
+                    <label>
+                        Артикул
+                    </label>
+                    <input id="article" name="article" type="text" required>
+                </div>
+                <div class="input-box">
+                    <label>
+                        Название
+                    </label>
+                    <input id="name" name="name" type="text" required>
+                </div>
+                <div class="input-box">
+                    <label>
+                        Статус
+                    </label>
+                    <select id="status" name="status">
+                        <option value="available">Доступен</option>
+                        <option value="unavailable">Недоступен</option>
+                    </select>
+                    <label style="font-weight: 900; margin-top: 10px">
+                        Атрибуты
+                    </label>
+                </div>
+                <div id="attributesInputContainer"></div>
+                <button type="button" onclick="generateInputs()" class="add-attribute">
+                    + Добавить атрибут
+                </button>
+                <button type="submit" class="save-button">
+                    Добавить
+                </button>
+            </form>
         </div>
     </div>
 </x-app-layout>
 <style>
-    .topbar {
-        position: relative;
-        top: 0;
-        left: 0;
-        width: 100%;
-        box-sizing: border-box;
-        padding: 0 20px;
-        background-color: white;
-    }
-    .sections {
-        align-items: center;
-        display: flex;
-        justify-content: space-between;
-        gap: 20px;
-        .section {
-            display: flex;
-            align-items: center;
-            gap: 20px;
-            a {
-                padding: 25px 0;
-                text-transform: uppercase;
-            }
-            a.active {
-                color: #ED1C24;
-                border-bottom: 4px solid #ED1C24;
-            }
-        }
-    }
-    table {
-        border-collapse: collapse;
-        margin-top: 20px;
-        width: 50vw;
-        min-width: 50vw;
-    }
-    th, td {
-        padding: 8px;
-        text-align: left;
-        font-weight: 400;
-        color: #6E6E6F;
-        border-bottom: 1px solid #C4C4C4;
-    }
-    tr.tr-body {
-        box-sizing: border-box;
-        padding: 10px;
-        background-color: white;
-    }
-    .add-button {
-        margin-top: 20px;
-        margin-right: 20px;
-        background-color: #0dc5fe;
-        color: white;
-        padding: 5px 40px;
-        border-radius: 7px;
-    }
     .save-button {
         margin-top: 20px;
         margin-right: 20px;
@@ -152,13 +53,6 @@
         padding: 10px 80px;
         border-radius: 7px;
         display: block;
-    }
-    .table-content {
-        display: flex;
-        align-items: flex-start;
-        justify-content: space-between;
-        gap: 20px;
-        width: 100%;
     }
     .add-block {
         background-color: #374050;
